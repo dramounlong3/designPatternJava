b3591c51e2add9cfa210dc2b1df541d4862727de
BANK\120791 <dramounlong3@sinopac.com>
2024-02-16 10:59:46 +0800

    git ignore

--- src/ch15_Builder/RootBuilderTest.java
+++ src/ch15_Builder/RootBuilderTest.java
@@ -12,6 +12,6 @@ public class RootBuilderTest {
         System.out.println("==========建造者模式測試==========");
         Director director = new Director(new GundamBuilder());  //GundamBuilder內有三種建造方法
         IRobot robot = director.builderRobot();                 //透過定義好的builderRobot流程建造機器人, 最後統一定義實體機器人Gundam(型態為IRobot)
-        robot.display();
+        robot.display();                                        //展示
     }
 }
