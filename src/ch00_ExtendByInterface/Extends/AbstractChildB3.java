ac6c9636f93458643ecd01326af899a8e8bb5e41
Kyle <dramounlong3@gmail.com>
2024-02-18 21:19:59 +0800

    [inheritance]

--- src/ch00_ExtendByInterface/Extends/AbstractChildB3.java
+++ src/ch00_ExtendByInterface/Extends/AbstractChildB3.java
@@ -1,4 +1,4 @@
-package src.ch00_ExtendByInterface;
+package src.ch00_ExtendByInterface.Extends;
 
 import src.ch00_ExtendByInterface.Extends.ParentClassA1;
 
@@ -6,5 +6,5 @@ import src.ch00_ExtendByInterface.Extends.ParentClassA1;
  * 抽象類別跟類別的繼承規則方式一樣, 只可以單一繼承, 對象可以是一般類別或抽象類別
  * 也可以implements多個interface
  */
-public abstract class AbstractChildB3 extends ParentClassA1 {
+public abstract class AbstractChildB3 extends ParentClassA1 implements ParentInterfaceA3, ParentInterfaceA4 {
 }
--- src/ch00_ExtendByInterface/Extends/ChildInterfaceB1.java
+++ src/ch00_ExtendByInterface/Extends/ChildInterfaceB1.java
@@ -1,5 +1,5 @@
 package src.ch00_ExtendByInterface.Extends;
 
-//interface只能繼承interface且可以多成繼承, 但不能繼承class, abstract 或 implements
+//interface只能繼承interface且可以有多個繼承, 但不能繼承class, abstract 或 implements
 public interface ChildInterfaceB1 extends ParentInterfaceA3, ParentInterfaceA4{
 }
